e95e6312e3e7c7d4ca60e9b10eb7cbfb3ead9fcd
jiangzhengnan <jiangzhengnan.jzn@alibaba-inc.com>
2022-03-02 20:58:23 +0800

    add.

--- app/src/main/java/com/ng/ngleetcode/MainActivity.java
+++ app/src/main/java/com/ng/ngleetcode/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ng.ngleetcode;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
@@ -11,36 +14,43 @@ import androidx.annotation.Nullable;
 
 import com.ng.ngleetcode.util.ProgressUtil;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class MainActivity extends Activity {
 
-    WebView mWebMain;
     TextView mShowTv;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mWebMain = findViewById(R.id.web_main);
         mShowTv = findViewById(R.id.tv_show);
         findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String proLink = ProgressUtil.getRandomProblemLink();
-                mShowTv.setText(proLink);
-                mWebMain.loadUrl(proLink);
+                //String proLink = ProgressUtil.getRandomProblemLink();
             }
         });
-        mWebMain.loadUrl("https://www.baidu.com/");
-
-        //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
-        mWebMain.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                //使用WebView加载显示url
-                view.loadUrl(url);
-                //返回true
-                return true;
-            }
-        });
+        mShowTv.setText(getFromAssets("StartCode.java"));
     }
+
+    public String getFromAssets(String fileName){
+        try {
+            InputStreamReader inputReader = new InputStreamReader( getResources().getAssets().open(fileName) );
+            BufferedReader bufReader = new BufferedReader(inputReader);
+            String line="";
+            String Result="";
+            while((line = bufReader.readLine()) != null) {
+                Result += line;
+                Result += "\n";
+            }
+
+            return Result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
 }
