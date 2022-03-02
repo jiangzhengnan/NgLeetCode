19168841c64deda06be8b6d238e9a8a6ab33c58b
jiangzhengnan <3442771773@qq.com>
2022-03-02 08:44:59 +0800

    add

---
+++ app/src/main/java/com/ng/ngleetcode/code/链表/链表的奇偶重排.java
@@ -0,0 +1,96 @@
+package com.ng.ngleetcode.code.链表;
+
+import com.ng.ngleetcode.util.ListNode;
+import com.ng.ngleetcode.util.LogUtil;
+import com.ng.ngleetcode.util.Solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 日期:
+ * 原题链接:https://www.nowcoder.com/practice/02bf49ea45cd486daa031614f9bd6fc3?tpId=295&tqId=1073463&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj%3Ftab%3D%25E7%25AE%2597%25E6%25B3%2595%25E7%25AF%2587%26topicId%3D295
+ * 原题描述:
+ * 描述
+ * 给定一个单链表，请设定一个函数，将链表的奇数位节点和偶数位节点分别放在一起，重排后输出。
+ * 注意是节点的编号而非节点的数值。
+ * <p>
+ * 数据范围：节点数量满足 0 <= n <= 10^50≤n≤10
+ * 5
+ * ，节点中的值都满足 0 <= val <= 10000≤val≤1000
+ * 要求：空间复杂度 O(n)O(n)，时间复杂度 O(n)O(n)
+ */
+@Solution(easy = 1, hard = 0)
+public class 链表的奇偶重排 {
+
+    public static void main(String[] args) {
+        ListNode data = ListNode.getNodeList(2, 1, 3, 5, 6, 4, 7);
+
+        LogUtil.pring(SolutionHard.oddEvenList(data));
+    }
+
+    private static class SolutionEasy {
+
+        /**
+         * 1.取出奇节点数组
+         * 2.取出偶节点数组
+         * 3.拼起来
+         */
+        public static ListNode oddEvenList(ListNode head) {
+            // write code here
+            List<ListNode> jiList = new ArrayList<>();
+            List<ListNode> ouList = new ArrayList<>();
+            int nowWei = 1;
+            while (head != null) {
+                if (nowWei % 2 == 0) {
+                    ouList.add(head);
+                } else {
+                    jiList.add(head);
+                }
+                head = head.next;
+                nowWei++;
+            }
+
+            ListNode ya = new ListNode(-1);
+            head = ya;
+            for (ListNode temp : jiList) {
+                head.next = new ListNode(temp.val);
+                head = head.next;
+            }
+            for (ListNode temp : ouList) {
+                head.next = new ListNode(temp.val);
+                head = head.next;
+            }
+
+            return ya.next;
+        }
+
+    }
+
+    private static class SolutionHard {
+
+        /**
+         * 1.odd节点指向奇数位 even节点指向偶数位
+         * 2.分别拼接奇数位链表和偶数位链表
+         * 3.合并两条链表
+         */
+        public static ListNode oddEvenList(ListNode head) {
+            if (head == null) {
+                return head;
+            }
+            ListNode evenHead = head.next;
+            ListNode odd = head;
+            ListNode even = evenHead;
+            while (even != null && even.next != null) {
+                odd.next = even.next;
+                odd = odd.next;
+                even.next = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+            return head;
+        }
+    }
+
+
+}
--- app/src/main/java/com/ng/ngleetcode/start/StartCode.java
+++ app/src/main/java/com/ng/ngleetcode/start/StartCode.java
@@ -1,5 +1,8 @@
 package com.ng.ngleetcode.start;
 
+import com.ng.ngleetcode.util.LogUtil;
+import com.ng.ngleetcode.util.ProgressUtil;
+
 /**
  * 随机抽题模拟器
  */
@@ -7,7 +10,7 @@ public class StartCode {
 
     public static void main(String[] args) {
         //当前进度
-        //ProgressUtil.getNowProgress();
+        ProgressUtil.getNowProgress();
 
         //随机得到几个题目
         //ProblemUtil.getRandomTest(5);
@@ -15,7 +18,7 @@ public class StartCode {
         //输出目录到readme
         //ProgressUtil.refreshReadMe();
 
-        //LogUtil.pring(ProgressUtil.getRandomProblemLink());
+        LogUtil.pring(ProgressUtil.getRandomProblemLink());
     }
 
 }
