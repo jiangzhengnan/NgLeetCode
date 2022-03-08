878d7f035538acc18d5706160124eb1fc8b6a285
jiangzhengnan <3442771773@qq.com>
2022-03-08 23:22:12 +0800

    add

---
+++ code/src/main/java/com/ng/code/menu/二叉树/二叉搜索树与双向链表.java
@@ -0,0 +1,54 @@
+package com.ng.code.menu.二叉树;
+
+import com.ng.code.util.Solution;
+import com.ng.code.util.TreeNode;
+
+/**
+ * 日期:
+ * 原题链接:https://www.nowcoder.com/practice/947f6eb80d944a84850b0538bf0ec3a5?tpId=295&tqId=23253&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj%3Ftab%3D%25E7%25AE%2597%25E6%25B3%2595%25E7%25AF%2587%26topicId%3D295
+ * 原题描述:
+ * 描述
+ * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。如下图所示
+ * <p>
+ * 数据范围：输入二叉树的节点数 0 \le n \le 10000≤n≤1000，二叉树中每个节点的值 0\le val \le 10000≤val≤1000
+ * 要求：空间复杂度O(1)O(1)（即在原树上操作），时间复杂度 O(n)O(n)
+ * <p>
+ * 注意:
+ * 1.要求不能创建任何新的结点，只能调整树中结点指针的指向。当转化完成以后，树中节点的左指针需要指向前驱，树中节点的右指针需要指向后继
+ * 2.返回链表中的第一个节点的指针
+ * 3.函数返回的TreeNode，有左右指针，其实可以看成一个双向链表的数据结构
+ * 4.你不用输出双向链表，程序会根据你的返回值自动打印输出
+ * <p>
+ * 输入描述：
+ * 二叉树的根节点
+ * 返回值描述：
+ * 双向链表的其中一个头节点。
+ * 示例1
+ * 输入：
+ * {10,6,14,4,8,12,16}
+ * 返回值：
+ * From left to right are:4,6,8,10,12,14,16;From right to left are:16,14,12,10,8,6,4;
+ * 说明：
+ * 输入题面图中二叉树，输出的时候将双向链表的头节点返回即可。
+ */
+@Solution(easy = 0, hard = 0)
+public class 二叉搜索树与双向链表 {
+
+    public static void main(String[] args) {
+        TreeNode data = TreeNode.createTreeNode(10,6,14,4,8,12,16);
+    }
+
+    private static class EasySolution {
+
+        public static TreeNode Convert(TreeNode pRootOfTree) {
+
+            return null;
+        }
+
+    }
+
+    private static class HardSolution {
+
+    }
+
+}
