7edec143da77425586b13682a334f5207ea38319
jiangzhengnan <3442771773@qq.com>
2022-09-14 15:11:57 +0800

    add

--- code/src/main/java/com/ng/code/设计模式/PracticeClass.java
+++ code/src/main/java/com/ng/code/设计模式/PracticeClass.java
@@ -1,13 +1,18 @@
 package com.ng.code.设计模式;
 
 
-import com.ng.code.设计模式.单例模式.Singleton4;
-import com.ng.code.设计模式.单例模式.Singleton5;
+import com.ng.code.util.LogUtil;
 
 public class PracticeClass {
 
+
     public static void main(String[] args) {
+
+        for (int i = 30; i >= 0; i--) {
+            LogUtil.pring(" " + i);
+        }
     }
+
 
 }
 
---
+++ code/src/main/java/com/ng/code/设计模式/工厂模式/依赖注入容器/NoSuchBeanDefinitionException.java
@@ -0,0 +1,10 @@
+package com.ng.code.设计模式.工厂模式.依赖注入容器;
+
+/**
+ * @author : jiangzhengnan.jzn@alibaba-inc.com
+ * @creation : 2022/08/14
+ * @description :
+ */
+public class NoSuchBeanDefinitionException extends Throwable {
+    public NoSuchBeanDefinitionException(final String s) {}
+}
