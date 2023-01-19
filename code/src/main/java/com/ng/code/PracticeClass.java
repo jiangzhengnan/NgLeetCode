5c28f52a10a18ec61a08c5b0faf3d115a356b65c
jiangzhengnan <3442771773@qq.com>
2023-01-19 15:15:46 +0800

    ee

--- code/src/main/java/com/ng/code/PracticeClass.java
+++ code/src/main/java/com/ng/code/PracticeClass.java
@@ -6,19 +6,13 @@ public class PracticeClass {
 
     public static void main(String[] args) {
         int[] data = new int[]{9, 8, 7, 3, 2, 1, 4, 5, 6};
-        sort(data);
+        sort(data, 0, data.length - 1);
         LogUtil.pring(data);
     }
 
-    /**
-     * 第一个元素默认已排序
-     * 遍历0到length-1的下标元素
-     * 每个元素每一次从i到0位置遍历，遇到比自己大的就后挪
-     * ，比自己小的就留在当前位置
-     */
-    private static void sort(final int[] data) {
+    private static void sort(int[] array, int start, int end) {
+        
     }
-
 
 }
 
