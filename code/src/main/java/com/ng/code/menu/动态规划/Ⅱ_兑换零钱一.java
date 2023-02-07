aa0475d154797e0d798434d1e8b6a51f194bf372
jiangzhengnan <3442771773@qq.com>
2023-02-07 20:22:25 +0800

    add

--- code/src/main/java/com/ng/code/menu/动态规划/Ⅱ_兑换零钱一.java
+++ code/src/main/java/com/ng/code/menu/动态规划/Ⅱ_兑换零钱一.java
@@ -3,6 +3,8 @@ package com.ng.code.menu.动态规划;
 import com.ng.code.util.LogUtil;
 import com.ng.code.util.Solution;
 
+import java.util.Arrays;
+
 /**
  * 原题描述:
  * https://leetcode.cn/problems/coin-change/?favorite=2ckc81c
@@ -10,66 +12,49 @@ import com.ng.code.util.Solution;
  * 如果无解，请返回-1.
  * 数据范围：数组大小满足 0 \le n \le 100000≤n≤10000 ， 数组中每个数字都满足 0 < val \le 100000<val≤10000，0 \le aim \le 50000≤aim≤5000
  * 要求：时间复杂度 O(n \times aim)O(n×aim) ，空间复杂度 O(aim)O(aim)。
+ * 示例1：
  * <p>
- * 示例:
- * 示例1
- * 输入：
- * [5,2,3],20
- * 返回值：
- * 4
+ * 输入：coins = [1, 2, 5], amount = 11
+ * 输出：3
+ * 解释：11 = 5 + 5 + 1
+ * 示例 2：
  * <p>
- * 示例2
- * 输入：
- * [5,2,3],0
- * 返回值：
- * 0
+ * 输入：coins = [2], amount = 3
+ * 输出：-1
+ * 示例 3：
+ * <p>
+ * 输入：coins = [1], amount = 0
+ * 输出：0
  */
 @Solution(easy = 0, hard = 0, partice = 0)
 public class Ⅱ_兑换零钱一 {
 
     public static void main(String[] args) {
-        LogUtil.pring(EasySolution.minMoney(new int[]{3,5,7}, 11));
+        LogUtil.pring(EasySolution.coinChange(new int[]{2}, 3));
 
     }
 
     private static class EasySolution {
 
         /**
-         * 最少货币数
-         *
-         * @param arr int整型一维数组 the array
-         * @param aim int整型 the target
-         * @return int整型
+         * https://leetcode.cn/problems/coin-change/solution/322-ling-qian-dui-huan-by-leetcode-solution/
+         * @param coins
+         * @param amount
+         * @return
          */
-        public static int minMoney(int[] arr, int aim) {
-            // write code here
-            //如何使用最少arr元素 构成 aim值
-            //dp[i] 代表给定钱数为i的时候最少货币数 就是凑成 i 元钱，需要dp[i] 张arr中面值纸币
-            //没办法兑换 arr[i]  dp[i] = dp[i]
-            //可以dp[i] = dp[i - arr[i]] + 1
-            //dp[i] = min(dp[i], dp[i-a[j]])
-            if (arr == null || arr.length == 0) {
-                return -1;
-            }
-            int[] dp = new int[aim + 1];
-            for (int i = 0; i <= aim; i++) {
-                dp[i] = aim + 1;
-            }
-            LogUtil.pring(dp);
-
+        public static int coinChange(int[] coins, int amount) {
+            int max = amount +1 ;
+            int[] dp = new int[amount + 1];
+            Arrays.fill(dp, max);
             dp[0] = 0;
-            for (int i = 1; i <= aim; i++) {
-                for (int j = 0; j < arr.length; j++) {
-                    if (arr[j] <= i) {
-                        //给了一张 3 元钱，小于 需要找零的4 元钱，那 就等于 1 + 需要找零剩下的钱dp[i -arr[j]] 4 - 3
-                        int now = dp[i];
-                        int zhao = dp[i - arr[j]] + 1;
-                        dp[i] = Math.min(now, zhao);
+            for (int i = 1; i <= amount; i++) {
+                for (int j = 0; j < coins.length; j++) {
+                    if (coins[j] <= i) {
+                        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                     }
                 }
             }
-            LogUtil.pring(dp);
-            return (dp[aim] > aim) ? -1 : dp[aim];
+            return dp[amount] > amount ? -1 : dp[amount];
         }
 
 
