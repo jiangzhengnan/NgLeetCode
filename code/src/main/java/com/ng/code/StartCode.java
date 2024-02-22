eec6f32eaf85529414f77048f57851c9993bd8c8
jiangzhengnan <jiangzhengnan@kuaishou.com>
2024-02-22 16:35:27 +0800

    add

--- code/src/main/java/com/ng/code/StartCode.java
+++ code/src/main/java/com/ng/code/StartCode.java
@@ -4,20 +4,31 @@ import com.ng.code.util.ProblemUtil;
 
 /**
  * 当前任务：
- * 1 Ⅰ_连续子数组的最大和 easy:0 particle:0
- * 2 Ⅰ_寻找峰值 easy:0 particle:0
- * 3 Ⅰ_二维数组中的查找 easy:0 particle:0
- * 4 Ⅰ_买卖股票的最好时机一 easy:0 particle:0
- * 5 Ⅰ_买卖股票的最好时机二 easy:0 particle:0
- * 6 Ⅰ_链表反转 easy:0 particle:0
- * 7 Ⅰ_包含min函数的栈 easy:0 particle:0
- * 8 Ⅰ_旋转数组 easy:0 particle:0
- * 9 Ⅰ_冒泡排序 easy:0 particle:0
- * 10 Ⅰ_有效括号序列 easy:0 particle:0
- * 11 Ⅰ_实现二叉树先序中序和后序遍历 easy:0 particle:0
- * 12 Ⅰ_Ⅱ_Ⅲ_TemplateTestClass easy:0 particle:0
- * 13 Ⅰ_最长公共前缀 easy:0 particle:0
- * 14 Ⅰ_跳台阶高级 easy:0 particle:0
+ * 1 Ⅰ_旋转数组 easy:0 particle:0
+ * 2 Ⅰ_连续子数组的最大和 easy:0 particle:0
+ * 3 Ⅰ_冒泡排序 easy:0 particle:0
+ * 4 Ⅰ_买卖股票的最好时机二 easy:0 particle:0
+ * 5 Ⅰ_生产者消费者模式 easy:0 particle:0
+ * 6 Ⅰ_有效括号序列 easy:0 particle:0
+ * 7 Ⅰ_买卖股票的最好时机一 easy:0 particle:0
+ * 8 Ⅰ_包含min函数的栈 easy:0 particle:0
+ * 9 Ⅰ_二分查找一 easy:0 particle:0
+ * 10 Ⅰ_删除有序链表中重复的元素一 easy:0 particle:0
+ * 11 Ⅰ_最长公共前缀 easy:0 particle:0
+ * 12 Ⅰ_比特位计数 easy:0 particle:0
+ * 13 Ⅰ_选择排序 easy:0 particle:0
+ * 14 Ⅰ_链表中倒数最后k个结点 easy:0 particle:0
+ * 15 Ⅰ_实现二叉树先序中序和后序遍历 easy:0 particle:0
+ * 16 Ⅰ_插入排序 easy:0 particle:0
+ * 17 Ⅰ_汉明距离 easy:0 particle:0
+ * 19 Ⅰ_判断是否为回文字符串 easy:0 particle:0
+ * 20 Ⅰ_链表反转 easy:0 particle:0
+ * 21 Ⅰ_跳台阶 easy:0 particle:0
+ * 22 Ⅰ_求二叉树的层序遍历 easy:0 particle:0
+ * 23 Ⅰ_跳台阶高级 easy:0 particle:0
+ * 24 Ⅰ_翻转单词序列 easy:0 particle:0
+ * 25 Ⅰ_二维数组中的查找 easy:0 particle:0
+ * 26 Ⅰ_寻找峰值 easy:0 particle:0
  */
 public final class StartCode {
 
@@ -26,7 +37,7 @@ public final class StartCode {
         ProblemUtil.refreshLocalProjects();
 
         //随机得到几个题目 (35 + 96 + 17)
-//        ProblemUtil.getRandomTest(14, 1);
+        //ProblemUtil.getRandomTest(50, 1);
     }
 
 
