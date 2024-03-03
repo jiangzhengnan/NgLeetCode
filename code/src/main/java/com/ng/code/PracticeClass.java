f141c12f01333dca82db3ea0aa0cc843f6e66100
jiangzhengnan <3442771773@qq.com>
2024-03-03 14:28:04 +0800

    add

--- code/src/main/java/com/ng/code/PracticeClass.java
+++ code/src/main/java/com/ng/code/PracticeClass.java
@@ -6,13 +6,25 @@ import com.ng.base.utils.LogUtil;
 public class PracticeClass {
 
     private void test() {
-        int[] data = new int[]{9, 8, 7, 3, 2, 1, 4, 5, 6};
+        int[] data = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1};
         sort(data);
         LogUtil.print(data);
     }
 
     private void sort(int[] data) {
+        //冒泡
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j + 1 < data.length - i; j++) {
+                if (data[j] > data[j + 1]) {
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+                LogUtil.print(data);
 
+            }
+
+        }
 
     }
 
