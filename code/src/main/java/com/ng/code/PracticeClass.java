dcb16754428eb315138f1c21f2e3950287db1d52
jiangzhengnan <3442771773@qq.com>
2024-05-14 21:01:56 +0800

    add

--- code/src/main/java/com/ng/code/PracticeClass.java
+++ code/src/main/java/com/ng/code/PracticeClass.java
@@ -3,6 +3,7 @@ package com.ng.code;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -13,72 +14,19 @@ import com.ng.base.utils.LogUtil;
 
 public class PracticeClass {
 
-
     private void test() {
-        ListNode phead1 = ListNode.getNodeList(1,2,3,4, 5);
-
-
-        LogUtil.print(reverseBetween(phead1, 2, 4));
-
+        LogUtil.print(swapPairs(ListNode.getNodeList(1, 2, 3, 4)));
     }
 
-    //快慢指针
+    public ListNode swapPairs(ListNode head) {
+        //分
+        ListNode slow = head;
+        ListNode fast = head;
 
-    /**
-     * 输入：
-     * {1,2,3,4,5},2,4
-     * 复制
-     * 返回值：
-     * {1,4,3,2,5}
-     * <p>
-     * 0   ... left  start  ... end  right ... n
-     *
-     * 3- 5
-     *
-     */
-    public ListNode reverseBetween(ListNode head, int m, int n) {
 
-        // write code here
-        ListNode
-            left = null,
-            right = head,
-            start = head,
-            end = null;
-        while (--m > 0) {
-            left = start;
-            start = start.next;
-        }
-        while (n-- > 0) {
-            end = right;
-            right = right.next;
-        }
-        if (right != null) {
-            end.next = null;
-        }
-        ListNode newHead = revers(start);
-        if (left != null) {
-            left.next = newHead;
-        } else {
-            head = newHead;
-        }
-        if (right != null) {
-            start.next = right;
-        }
+        //治
 
-        return head;
-    }
-
-    private ListNode revers(ListNode node) {
-        ListNode pre = null;
-        ListNode now = node;
-        ListNode next = null;
-        while (now != null) {
-            next = now.next;
-            now.next = pre;
-            pre = now;
-            now = next;
-        }
-        return pre;
+        return newHead;
     }
 
 
@@ -90,5 +38,25 @@ public class PracticeClass {
 }
 
 
+/*
+https://leetcode.cn/problems/swap-nodes-in-pairs/description/?envType=study-plan-v2&envId=top-100
+-liked-100-liked-liked
 
+给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。
+你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+输入：head = [1,2,3,4]
+输出：[2,1,4,3]
+示例 2：
 
+输入：head = []
+输出：[]
+示例 3：
+
+输入：head = [1]
+输出：[1]
+
+提示：
+
+链表中节点的数目在范围 [0, 100] 内
+0 <= Node.val <= 100
+ */
