5f18f8755ebdd3905dc937c36b662c5bb5ea507f
garyznjiang <garyznjiang@tencent.com>
2025-12-22 19:28:07 +0800

    fix 编译问题

--- app/src/main/java/com/ng/ngleetcode/ui/widgets/Commons.kt
+++ app/src/main/java/com/ng/ngleetcode/ui/widgets/Commons.kt
@@ -3,7 +3,13 @@ package com.ng.ngleetcode.ui.widgets
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
@@ -109,7 +115,7 @@ fun AppToolsBar(
 fun BottomNavBarView(navCtrl: NavHostController) {
     val bottomNavList = listOf(
         BottomNavRoute.Code,
-        BottomNavRoute.Statistics,
+//        BottomNavRoute.Statistics,
         BottomNavRoute.Read,
         BottomNavRoute.Profile
     )
